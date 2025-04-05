4cdf2b82f342a72fc22e99a21fb1fbff9c33563a
Minos <mindaugas.andrulevicius@gmail.com>
2025-04-05 16:32:42 +0300

    collisions

--- Assets/_MA/PlayerController.cs
+++ Assets/_MA/PlayerController.cs
@@ -4,13 +4,19 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    Camera _mainCamera;
+    
     [SerializeField] GameObject player;
     Entity _playerEntity;
+    
+    PlayerInput _playerInput;
     
     // ============================= INIT =============================
     void Awake()
     {
         _playerEntity = player.GetComponent<Entity>();
+        _playerInput = GetComponent<PlayerInput>();
+        _mainCamera = Camera.main;
     }
     
     // =========================== TRIGGERS ===========================
@@ -20,16 +26,33 @@ public class PlayerController : MonoBehaviour
         _playerEntity.moveDirectionVector = value.Get<Vector2>();
     }
 
-    // roll/dash
-    // [UsedImplicitly]
-    // void OnSprint(InputValue value)
-    // {
-    //     _playerEntity.isBoosting = value.isPressed;
-    // }
+    [UsedImplicitly]
+    void OnSprint(InputValue value)
+    {
+        _playerEntity.isSprinting = value.isPressed;
+    }
 
     [UsedImplicitly]
     void OnJump(InputValue value)
     {
-        _playerEntity.isJumping = value.isPressed;
+        _playerEntity.isDashing = value.isPressed;
+    }
+
+    [UsedImplicitly]
+    void OnLook(InputValue value)
+    {
+        if (value.Get<Vector2>() == Vector2.zero) return;
+        if(!player) return;
+        if (_playerInput.currentControlScheme == "Gamepad") _playerEntity.lookDirectionVector = value.Get<Vector2>();
+        else if (_playerInput.currentControlScheme == "Keyboard&Mouse")
+        {
+            _playerEntity.lookDirectionVector = (_mainCamera.ScreenToWorldPoint(Mouse.current.position.ReadValue())
+                                                 - player.transform.position).normalized;
+        }
+        else if (_playerInput.currentControlScheme == "Touch")
+        {
+            // lookDirectionVector based on touch position, like mouse
+            // (or control with touch wheel, like gamepad)
+        }
     }
 }
