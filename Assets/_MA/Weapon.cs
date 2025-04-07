6f87a882e532de5eb5555ce43b31b7435c66be48
Minos <mindaugas.andrulevicius@gmail.com>
2025-04-07 21:48:55 +0300

    fixxxes

--- Assets/_MA/Weapon.cs
+++ Assets/_MA/Weapon.cs
@@ -38,11 +38,14 @@ public class Weapon : MonoBehaviour
     void Shoot()
     {
         GetComponent<AudioSource>().Play();
-        GameObject bullet = Instantiate(bulletPrefab, transform.position, Quaternion.identity);
+        var bulletPos = transform.position;
+        if (bulletPrefab.gameObject.name == "Fart") bulletPos.z += 1;
+        else bulletPos.z -= 1;
+        GameObject bullet = Instantiate(bulletPrefab, bulletPos, Quaternion.identity);
         bullet.GetComponent<Entity>().stats.touchDamage = parentEntity.stats.rangedDamage;
         Vector2 direction = parentEntity.lookDirectionVector;
         if (gameObject.name == "LustWeapon") bullet.GetComponent<Entity>().movementTarget = parentEntity.lookTarget;
-        else bullet.GetComponent<Entity>().movementTargetPosition = parentEntity.lookTargetPosition;
+        else bullet.GetComponent<Entity>().moveDirectionVector = parentEntity.lookDirectionVector;
         // Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
         bullet.layer = gameObject.layer;
         // rb.linearVelocity = direction * shotForce + parentEntity.stats.velocity;
